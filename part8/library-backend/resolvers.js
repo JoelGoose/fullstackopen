0d90128ca3c0d6e1b69e22c6d3628f52896c596d
JoelGoose <joel.gasstrom@abo.fi>
2026-01-26 15:49:16 +0200

    completed exercises 8.13-8.15

--- part8/library-backend/resolvers.js
+++ part8/library-backend/resolvers.js
@@ -1,128 +1,61 @@
-const { v1: uuid } = require('uuid')
+const { GraphQLError } = require('graphql')
 
-
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
-
-/*
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
-*/
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'Demons',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
+const Book = require('./models/book')
+const Author = require('./models/author')
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: (root, args) => {
-      // bad approach in hindsight, but works so long as no more arguments get added
-      if (!args.author && !args.genre) return books
-      else if (args.author && args.genre) {
-        const booksByAuthor = books.filter(book => book.author === args.author)
-        return booksByAuthor.filter(book => book.genres.includes(args.genre))
-      }
-      else if (args.author && !args.genre) return books.filter(book => book.author === args.author)
-      else if (!args.author && args.genre) return books.filter(book => book.genres.includes(args.genre))
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+			if (args.genre) {
+				const books = await Book.find({ genres: args.genre})
+				return books
+			}
+			else
+				return Book.find({})
     },
-    allAuthors: () => authors
+    allAuthors: async () => Author.find({})
   },
   Author: {
-    bookCount: (root) => {
-      return books.filter(book => book.author === root.name).length
+    bookCount: (root) => { // NOT WORKING
+      // return books.filter(book => book.author === root.name).length
     }
   },
   Mutation: {
-    addBook: (root, args) => {
-      const book = { ...args, id: uuid() }
-      books = books.concat(book)
-      if (!authors.find(author => author.name === args.author)) {
-        const author = { name: args.author, id: uuid() }
-        authors = authors.concat(author)
-      }
-      return book
+    addBook: async (root, args) => {
+      let author = await Author.findOne({ name: args.author })
+			if (!author) {
+				author = new Author({ name: args.author })
+				try {
+					await author.save()
+				} catch (error) {
+					throw new GraphQLError(`Saving new author failed: ${error}`, {
+						extensions: {
+							code: 'BAD_USER_INPUT',
+							invalidArgs: args.name,
+							error
+						}
+					})
+				}
+			}
+			const book = new Book({ ...args, author: author })
+			try {
+				await book.save()
+			} catch (error) {
+				throw new GraphQLError(`Saving new book failed: ${error}`, {
+					extensions: {
+						code: 'BAD_USER_INPUT',
+						invalidArgs: args.title,
+						error
+					}
+				})
+			}
+			return book
     },
-    editAuthor: (root, args) => {
-      const author = authors.find(author => author.name === args.name)
-      if (!author) {
-        return null
-      }
-      const updatedAuthor = { ...author, born: args.setBornTo }
-      authors = authors.map(author => author.name === args.name ? updatedAuthor : author)
-      return updatedAuthor
+    editAuthor: async (root, args) => {
+      const author = await Author.findOneAndUpdate({ name: args.name }, { $set: { born: args.setBornTo }}, { new: true })
+			return author
     }
   }
 }
